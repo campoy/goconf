ed6d4d4cbfc31dfe2be2049c068faae871568273
Francesc Campoy <campoy@golang.org>
2013-07-30 10:25:08 -0700

    adding userprofile

---
+++ conference/userprofile.go
@@ -0,0 +1,97 @@
+package conference
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+
+	"appengine"
+	"appengine/datastore"
+	"appengine/user"
+)
+
+func init() {
+	http.Handle("/userprofile", handler(userProfileHandler))
+	http.Handle("/saveprofile", handler(saveProfileHandler))
+}
+
+type Conference struct {
+	Name      string
+	City      string
+	StartDate string
+}
+
+type UserProfile struct {
+	RegisteredConfs []Conference
+	Name            string
+	Topics          []string
+	MainEmail       string
+	NotifEmail      string
+}
+
+func (u UserProfile) SelectedTopic(topic string) bool {
+	for _, t := range u.Topics {
+		if t == topic {
+			return true
+		}
+	}
+	return false
+}
+
+func userProfileHandler(w io.Writer, r *http.Request) error {
+	ctx := appengine.NewContext(r)
+
+	u := user.Current(ctx)
+	if u == nil {
+		return fmt.Errorf("required login for userprofile")
+	}
+
+	data := struct {
+		User   UserProfile
+		Topics []string
+	}{Topics: topicList}
+
+	heading := "Edit your"
+
+	k := datastore.NewKey(ctx, "RegisteredUser", u.Email, 0, nil)
+	if err := datastore.Get(ctx, k, &data.User); err != nil {
+		if err != datastore.ErrNoSuchEntity {
+			return fmt.Errorf("get userprofile: %v", err)
+		}
+		heading = "Create a "
+		data.User.MainEmail = u.Email
+	}
+
+	p, err := NewPage(ctx, "userprofile", data)
+	if err != nil {
+		return fmt.Errorf("create userprofile page: %v", err)
+	}
+	p.Heading = heading
+	return p.Render(w)
+}
+
+func saveProfileHandler(w io.Writer, r *http.Request) error {
+	if r.Method != "POST" {
+		return RedirectTo("/userprofile")
+	}
+
+	ctx := appengine.NewContext(r)
+	u := user.Current(ctx)
+	if u == nil {
+		return fmt.Errorf("required login for userprofile")
+	}
+
+	up := UserProfile{
+		MainEmail:  u.Email,
+		Name:       r.FormValue("person_name"),
+		NotifEmail: r.FormValue("notification_email"),
+		Topics:     strings.Split(r.FormValue("topics"), " "),
+	}
+
+	k := datastore.NewKey(ctx, "RegisteredUser", u.Email, 0, nil)
+	if _, err := datastore.Put(ctx, k, &up); err != nil {
+		return fmt.Errorf("save userprofile: %v", err)
+	}
+	return RedirectTo("/userprofile")
+}
